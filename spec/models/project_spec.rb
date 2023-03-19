b6d592690982200378678621f366784cd01ce3ec
satoshitodaka <satoshitodaka0705@gmail.com>
2023-03-19 23:33:59 +0900

    test: 💍 Project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  # ユーザー単位では重複したプロジェクト名を許可しないこと
-  it "does not allow duplicate project names per user" do
-    user = User.create(
+  before do
+    @user = User.create(
       first_name: 'Joe',
       last_name: 'Tester',
       email: 'tester@example.com',
       password: 'password'
     )
 
-    user.projects.create(
+    @user.projects.create(
       name: 'Test Project'
     )
+  end
 
-    new_project = user.projects.build(
+  # ユーザー単位では重複したプロジェクト名を許可しないこと
+  it "does not allow duplicate project names per user" do
+    new_project = @user.projects.build(
       name: 'Test Project'
     )
 
@@ -24,17 +26,6 @@ RSpec.describe Project, type: :model do
 
   # 二人のユーザーが同じ名前を使うことは許可すること
   it "allows two users to share a project name" do
-    user = User.create(
-      first_name: 'Joe',
-      last_name: 'Tester',
-      email: 'joetester@example.com',
-      password: 'password'
-    )
-
-    user.projects.create(
-      name: 'Test Project'
-    )
-
     other_user = User.create(
       first_name: 'Jane',
       last_name: 'Tester',
