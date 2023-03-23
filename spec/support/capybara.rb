006b6c9cc2e77857db1e3ee2957da52c5510ebac
satoshitodaka <satoshitodaka0705@gmail.com>
2023-03-23 09:46:46 +0900

    refactor: 💡 change driver to headless

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,6 +3,6 @@ RSpec.configure do |config|
     driven_by :rack_test
   end
   config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome
+    driven_by :selenium_chrome_headless
   end
 end
